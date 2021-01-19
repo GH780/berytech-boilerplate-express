e8eaf0515c4256ce2e19828e0dd38c88ddf6f483
Gardenia <60855908+GH780@users.noreply.github.com>
2021-01-19 21:24:09 +0200

    Update usergroupList.js

--- controllers/usergroupList.js
+++ controllers/usergroupList.js
@@ -1,62 +1,75 @@
-const slackToken =process.env.token;
-const url = 'https://slack.com/api/usergroups.list';
-const axios = require('axios');
-const { MongoClient } = require("mongodb");
-var mongoDB =process.env.dburl;
-var UserGroup = require('../models/usrgroup');
-const uri ="mongodb://localhost:27017/";
-const client = new MongoClient(uri,{
+const slackToken = process.env.token;
+const axios = require("axios");
+var mongoose = require("mongoose");
+var mongoDB = process.env.dburl;
+mongoose.connect(mongoDB, {
     useNewUrlParser: true,
     useUnifiedTopology: true
-  });
+});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-var mongoose = require('mongoose');
-mongoose.connect(mongoDB, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+var UserGroup = require("../models/usrgroup");
 
 
-exports.fetchUsrGroupList = async function () {
-    
-    await client.connect();
-    const database = client.db("gardeniadb");
-    const collection = database.collection("UserGroup");
+
+
+exports.fetchUsrGroupList = async () => {
+    var message = "";
+    const collection = db.collection("UserGroup");
 
     //deactivate all user group status in database
-    const resul = await collection.updateMany({status: "active" }, {$set: {status: "deactive"} });
-    console.log(resul.result.nModified + " documents deactivate its status in database");
-
+    const updatedData = await collection.updateMany({
+        status: "active"
+    }, {
+        $set: {
+            status: "deactive"
+        }
+    });
     //call API to fetch usergroup list from slackworkspace
-    const res = await axios.post(url, {
-    }, { headers: { authorization: `Bearer ${slackToken}` } });
-    //console.log(url+' call done', res.data);
-    if(!res.data) return;
+    const res = await axios.post(
+        process.env.slackusergroupapi, {}, {
+            headers: {
+                authorization: `Bearer ${slackToken}`
+            }
+        }
+    );
 
+    if (!res.data) return;
     var arrUserGroups = res.data.usergroups;
-   
-    var message="";
+
     for (var i = 0; i < arrUserGroups.length; i++) {
-       var usrgroupName= arrUserGroups[i].name;
-       
-        query = {name: usrgroupName}
+        var usrgroupName = arrUserGroups[i].name;
+
+        let query = {
+            name: usrgroupName
+        };
 
         // check if usergroup name exist in local db
-       const findUG = await collection.findOne(query);
-        //if result == null || usergroup not exist
+        const findUG = await collection.findOne(query);
+
         // add to db and activate status
-        if (!findUG ){
-            await UserGroup.create({ name: usrgroupName, id: arrUserGroups[i].id, status: "active" },(err,data)=>{
-                message+= data.name+" this document is saved";
-                message+="  ";
-            });
-            //console.log(usrgroupName +  ' saved');
-        }else{
+        if (!findUG) {
+            await UserGroup.create({
+                    name: usrgroupName,
+                    id: arrUserGroups[i].id,
+                    status: "active"
+                },
+                (err, data) => {
+                    message += ` the ${ data.name} is saved <br/>`;
+
+                }
+            );
+        } else {
             //update usergroup status to active if already exist in local db
-            const update = await collection.updateOne(query, {$set: {status: "active"} });
-            message+=usrgroupName+ " activate its status";
-            message+="  ";
+            await collection.updateOne(query, {
+                $set: {
+                    status: "active"
+                },
+            });
+            message += `${usrgroupName} activate its status <br/> `;
+
         }
-   }
+    }
     return message;
-}
+};
